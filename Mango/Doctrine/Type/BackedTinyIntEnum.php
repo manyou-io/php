e9ff114852a39b321c401396fce45140a52a6669
Bohan <yang@bohan.co>
2022-11-12 11:27:08 +0000

    Fix

--- Mango/Doctrine/Type/BackedTinyIntEnum.php
+++ Mango/Doctrine/Type/BackedTinyIntEnum.php
@@ -6,40 +6,29 @@ namespace Manyou\Mango\Doctrine\Type;
 
 use BackedEnum;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use LogicException;
 
 use function array_map;
 use function is_a;
-use function sprintf;
 
 trait BackedTinyIntEnum
 {
     use ArrayTinyIntEnum;
 
-    private string $enumClass;
-
     abstract private function getEnumClass(): string;
-
-    public function __construct()
-    {
-        if (! is_a($this->enumClass = $this->getEnumClass(), BackedEnum::class, true)) {
-            throw new LogicException(sprintf('Enum class "%s" is not a BackedEnum.', self::class));
-        }
-    }
 
     private function getEnums(): array
     {
-        return array_map(static fn (BackedEnum $enum) => $enum->value, $this->enumClass::cases());
+        return array_map(static fn (BackedEnum $enum) => $enum->value, $this->getEnumClass()::cases());
     }
 
     public function convertToPHPValue($value, AbstractPlatform $platform): ?BackedEnum
     {
-        return $value === null ? null : $this->enumClass::from($value);
+        return $value === null ? null : $this->getEnumClass()::from($value);
     }
 
     public function valueToId($value): ?int
     {
-        if (! is_a($value, $this->enumClass)) {
+        if (! is_a($value, $this->getEnumClass())) {
             return null;
         }
 
@@ -50,6 +39,6 @@ trait BackedTinyIntEnum
     {
         $value = $this->getValueMap()[$id] ?? null;
 
-        return $value === null ? null : $this->enumClass::from($value);
+        return $value === null ? null : $this->getEnumClass()::from($value);
     }
 }
