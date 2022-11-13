d8d84ed07ade76f210a161e97ccdbd2b25a200d8
Brent, Bohan Yang <brent@manyou.io>
2022-11-13 10:09:41 +0000

    Bootstrap test env with MicroKernelTrait

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -5,14 +5,6 @@ declare(strict_types=1);
 use Symplify\MonorepoBuilder\Config\MBConfig;
 
 return static function (MBConfig $mbConfig): void {
-    $mbConfig->packageDirectories([
-        __DIR__ . '/Aria2',
-        __DIR__ . '/BingHomepage',
-        __DIR__ . '/LeanStorage',
-        __DIR__ . '/Mango',
-        __DIR__ . '/PromiseHttpClient',
-        __DIR__ . '/RemoveDataCollectorBundle',
-        __DIR__ . '/WorkermanSymfonyRuntime',
-    ]);
+    $mbConfig->packageDirectories([__DIR__ . '/src']);
     $mbConfig->defaultBranch('main');
 };
