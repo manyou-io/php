a409c79f680df368d54c4ae1a0533075601574d7
Bohan Yang <yang@bohan.co>
2023-05-03 12:23:41 +0000

    Update

--- src/Mango/TaskQueue/Doctrine/Type/TaskStatusType.php
+++ src/Mango/TaskQueue/Doctrine/Type/TaskStatusType.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Manyou\Mango\TaskQueue\Doctrine\Type;
 
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\Type;
 use Manyou\Mango\Doctrine\Type\BackedEnumType;
 use Manyou\Mango\TaskQueue\Enum\TaskStatus;
@@ -22,5 +23,10 @@ class TaskStatusType extends Type
     private function getEnumClass(): string
     {
         return TaskStatus::class;
+    }
+
+    private function usingTinyInt(AbstractPlatform $platform): bool
+    {
+        return true;
     }
 }
