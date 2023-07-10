e05028b25b20b0d95ccfd366cf7c14dc07880a8c
Bohan Yang <brent@manyou.io>
2023-07-10 00:54:21 +0000

    Update

--- packages/clear-service-tags-bundle/DependencyInjection/ClearServiceTagsExtension.php
+++ packages/clear-service-tags-bundle/DependencyInjection/ClearServiceTagsExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class ClearServiceTagsExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration(new Configuration(), $configs);
 
